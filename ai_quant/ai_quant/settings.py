679ef3fe5aae9be2d6d2d4627fb6e53430810896
hee_!J <heeej920@naver.com>
2023-09-19 23:17:14 +0900

    Update settings.py

--- ai_quant/ai_quant/settings.py
+++ ai_quant/ai_quant/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'quant.apps.QuantConfig',
+    'rest_framework',
+    'corsheaders',      # CORS 관련 추가
 ]
 
 MIDDLEWARE = [
